f828066ba4d35b30fc37e11d00f59b49c15e56f7
Egoist <epsilonelittr@gmail.com>
2022-12-20 07:04:46 +0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { ChangeDetectorRef, Component } from '@angular/core';
-import ReactFlow, { Background, Controls } from 'reactflow';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,42 +6,7 @@ import ReactFlow, { Background, Controls } from 'reactflow';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  public Flow: any = ReactFlow;
+  public constructor() {}
 
-  public props: any = {
-    edges: [
-      { id: '1-2', source: '1', target: '2', label: 'to the', type: 'step' },
-    ],
-
-    nodes: [
-      {
-        id: '1',
-        data: { label: 'Hello' },
-        position: { x: 0, y: 0 },
-        type: 'input',
-      },
-      {
-        id: '2',
-        data: { label: 'World' },
-        position: { x: 100, y: 100 },
-      },
-    ],
-  };
-
-  public constructor(private changeDetectorRef: ChangeDetectorRef) {}
-
-  onNgInit() {
-    // nested components
-    this.Flow.Background = Background;
-    this.Flow.Controls = Controls;
-
-    this.Flow.defaultStyles = {
-      background: {
-        fill: '#aaa',
-        stroke: '#555',
-      },
-    };
-
-    this.changeDetectorRef.detectChanges();
-  }
+  onNgInit() {}
 }
