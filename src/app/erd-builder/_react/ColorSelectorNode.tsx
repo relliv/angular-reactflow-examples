14830d7026b50c9ac681cfec6337bd98474c77c1
Egoist <epsilonelittr@gmail.com>
2023-06-15 04:53:47 +0300

    Add simple entity example

--- src/app/erd-builder/_react/ColorSelectorNode.tsx
+++ src/app/erd-builder/_react/ColorSelectorNode.tsx
@@ -2,31 +2,81 @@ import React, { memo } from "react";
 import { Handle, Position } from "reactflow";
 
 export default memo(({ data, isConnectable }: any) => {
+  const fields = [
+    {
+      id: "1",
+      icon: "+",
+      name: "field_name 1",
+      type: "field_type",
+    },
+    {
+      id: "2",
+      icon: "+",
+      name: "field_name 2",
+      type: "field_type",
+    },
+    {
+      id: "3",
+      icon: "+",
+      name: "field_name 3",
+      type: "field_type",
+    },
+    {
+      id: "4",
+      icon: "+",
+      name: "field_name 4",
+      type: "field_type",
+    },
+    {
+      id: "5",
+      icon: "+",
+      name: "field_name 5",
+      type: "field_type",
+    },
+    {
+      id: "6",
+      icon: "+",
+      name: "field_name 6",
+      type: "field_type",
+    },
+  ];
+
   return (
     <div className="erd-entity">
-      <Handle
-        type="target"
-        position={Position.Left}
-        style={{ background: "#555" }}
-        onConnect={(params) => console.log("handle onConnect", params)}
-        isConnectable={isConnectable}
-      />
-      <div>
-        Custom Color Picker Node: <strong>{data.color}</strong>
-        <Handle
-          type="source"
-          position={Position.Right}
-          id="a"
-          style={{ top: 10, background: "#555" }}
-          isConnectable={isConnectable}
-        />
-        <Handle
-          type="source"
-          position={Position.Right}
-          id="b"
-          style={{ bottom: 10, top: "auto", background: "#555" }}
-          isConnectable={isConnectable}
-        />
+      {/* Header */}
+      <div className="header">table_name_name_name</div>
+
+      {/* Body */}
+      <div className="body">
+        {fields.map((field, index) => (
+          <div className="field">
+            {/* Left Handle */}
+            <Handle
+              type="target"
+              position={Position.Left}
+              style={{ background: "#555" }}
+              onConnect={(params) => console.log("handle onConnect", params)}
+              isConnectable={isConnectable}
+            />
+
+            {/* Icon */}
+            <div className="field-icon">{field.icon}</div>
+
+            {/* Name */}
+            <div className="field-name">{field.name}</div>
+
+            {/* Type */}
+            <div className="field-type">{field.type}</div>
+
+            {/* Right Handle */}
+            <Handle
+              id={field.id}
+              type="source"
+              position={Position.Right}
+              isConnectable={isConnectable}
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
